4de1b58788f4542e0b52437bd87f776d11e6e8c2
shannonstevens73 <shannon.stevens@bryanuniversity.edu>
2020-07-30 16:19:58 -0500

    first commit

---
+++ week1/todoPart1/my-app/src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function App() {
+    return(
+
+        <div>
+
+            <input type="checkbox" />
+            <span>Placeholder text</span><br /><br />
+
+            <input type="checkbox" />
+            <span>Placeholder text</span><br /><br />
+
+            <input type="checkbox" />
+            <span>Placeholder text</span><br /><br />
+            
+        </div>
+    )
+}
+
+export default App
