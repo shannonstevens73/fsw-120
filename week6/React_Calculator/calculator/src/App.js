4efc7f8c12059b82b0410fde29f2d73558d3a6a6
shannonstevens73 <shannon.stevens@bryanuniversity.edu>
2020-09-05 13:07:03 -0500

    second commit

--- week6/React_Calculator/calculator/src/App.js
+++ week6/React_Calculator/calculator/src/App.js
@@ -4,32 +4,28 @@ import './index.css'
 function App() {
   const [number1, setNumber1] = useState(0)
   const [number2, setNumber2] = useState(0)
-  const [addTotal, setAddTotal] = useState(number1 + number2)
-  const [subTotal, setSubTotal] = useState(number1 - number2)
-  const [mulTotal, setMulTotal] = useState(number1 * number2)
-  const [divTotal, setDivTotal] = useState(number1 + number2)
-  // const [total, setTotal] = useState
+  const [total, setTotal] = useState(0)
 
   function calculateAddTotal() {
-    setAddTotal(number1 + number2)
+    setTotal(number1 + number2)
     setNumber1(0)
     setNumber2(0)
   }
 
   function calculateSubTotal() {
-    setSubTotal(number1 - number2)
+    setTotal(number1 - number2)
     setNumber1(0)
     setNumber2(0)
   }
 
   function calculateMulTotal() {
-    setMulTotal(number1 * number2)
+    setTotal(number1 * number2)
     setNumber1(0)
     setNumber2(0)
   }
 
   function calculateDivTotal() {
-    setDivTotal(number1 / number2)
+    setTotal(number1 / number2)
     setNumber1(0)
     setNumber2(0)
   }
@@ -38,37 +34,36 @@ function App() {
     <div className="App">
       <h1>React Calculator</h1>
 
+      <br />
+
       <div className="number-inputs">
         <input
+          id="input1"
           type="number"
           value={number1}
           onChange={e => setNumber1(+e.target.value)}
         />
         <input
+          id="input2"
           type="number"
           value={number2}
           onChange={e => setNumber2(+e.target.value)}
         />
       </div>
 
-      <button onClick={calculateAddTotal}>Add</button>
-      <button onClick={calculateSubTotal}>Subtract</button>
-      <button onClick={calculateMulTotal}>Multiply</button>
-      <button onClick={calculateDivTotal}>Divide</button>
-
       <br /><br />
 
-      <span>
-        {addTotal}
-        {subTotal}
-        {mulTotal}
-        {divTotal}  
-      </span>   
+      <div id="buttons">
+        <button id="btn1" onClick={calculateAddTotal}>Add</button>
+        <button id="btn2" onClick={calculateSubTotal}>Subtract</button>
+        <button id="btn3" onClick={calculateMulTotal}>Multiply</button>
+        <button id="btn4" onClick={calculateDivTotal}>Divide</button>
+      </div>
+
+      <h1 id="total">Answer: {total}</h1>   
 
     </div>
   )
 }
 
-
-
-export default App;
+export default App
